f0e3805c0f1c9923ef1443059c5d71f04c342b64
Stefan Strableg <darkspark65@gmail.com>
2024-01-08 13:20:35 +0100

    Prettier Printing

--- Basics.cs
+++ Basics.cs
@@ -12,53 +12,54 @@ namespace LL_CS
     {
         static void testLinkedList(IHLContainer ll)
         {
-            Console.WriteLine("LinkedList has " + ll.Count() + " elements:");
+            Console.WriteLine("\n\n### testLinkedList");
+            Console.WriteLine("\nLinkedList has " + ll.Count() + " elements:");
             ll.Print();
 
-            Console.WriteLine("Adding to head:");
+            Console.WriteLine("\nAdding to head:");
             ll.AddHead("head");
             Console.WriteLine("(" + ll.Count() + " elements)");
             ll.Print();
 
 
-            Console.WriteLine("Adding to tail:");
+            Console.WriteLine("\nAdding to tail:");
             ll.AddTail("tail");
             Console.WriteLine("(" + ll.Count() + " elements)");
             ll.Print();
 
-            Console.WriteLine("Inserting to middle:");
+            Console.WriteLine("\nInserting to middle:");
             ll.InsertAtPos("middle", ll.Count() / 2);
             ll.InsertAtPos("middle", ll.Count() / 2);
             Console.WriteLine("(" + ll.Count() + " elements)");
             ll.Print();
 
-            Console.WriteLine("Finding element:");
+            Console.WriteLine("\nFinding element:");
             Console.WriteLine("head: " + ll.Find("head", StringComparer.Ordinal));
             Console.WriteLine("tail: " + ll.Find("tail", StringComparer.Ordinal));
             Console.WriteLine("middle: " + ll.Find("middle", StringComparer.Ordinal));
 
-            Console.WriteLine("Removing head:");
+            Console.WriteLine("\nRemoving head:");
             ll.RemoveHead();
             Console.WriteLine("(" + ll.Count() + " elements)");
             ll.Print();
 
-            Console.WriteLine("Removing tail:");
+            Console.WriteLine("\nRemoving tail:");
             ll.RemoveTail();
             Console.WriteLine("(" + ll.Count() + " elements)");
             ll.Print();
 
-            Console.WriteLine("Removing middle:");
+            Console.WriteLine("\nRemoving middle:");
             ll.RemoveAt(ll.Count()/2);
             Console.WriteLine("(" + ll.Count() + " elements)");
             ll.Print();
 
-            Console.WriteLine("Find and remove:");
+            Console.WriteLine("\nFind and remove:");
             ll.Remove("middle");
             Console.WriteLine("(" + ll.Count() + " elements)");
             ll.Print();
 
 
-            Console.WriteLine("Iterating with First() and Next():");
+            Console.WriteLine("\nIterating with First() and Next():");
 
             int i = 0;
             for (object o = ll.First(); o != null; o = ll.Next())
@@ -67,20 +68,21 @@ namespace LL_CS
                 i++;
             }
 
-            Console.WriteLine("Finished testing!");
+            Console.WriteLine("\nFinished testing!");
         }
         static void testLinkedList_edgecases()
         {
+            Console.WriteLine("\n\n### testLinkedList_edgecases");
             LinkedList ll = new LinkedList();
 
-            Console.WriteLine("Removing Head of empty list");
+            Console.WriteLine("\nRemoving Head of empty list");
             try { 
                 ll.RemoveHead();
             } catch (Exception e) 
             {
                 Console.WriteLine(e.Message);
             }
-            Console.WriteLine("Removing Tail of empty list");
+            Console.WriteLine("\nRemoving Tail of empty list");
             try
             {
                 ll.RemoveTail();
@@ -89,7 +91,7 @@ namespace LL_CS
             {
                 Console.WriteLine(e.Message);
             }
-            Console.WriteLine("Removing nonexistent element");
+            Console.WriteLine("\nRemoving nonexistent element");
             try
             {
                 ll.Remove("nonexistent");
@@ -99,7 +101,7 @@ namespace LL_CS
                 Console.WriteLine(e.Message);
             }
 
-            Console.WriteLine("Iterating empty LinkedList with First() and Next():");
+            Console.WriteLine("\nIterating empty LinkedList with First() and Next():");
 
             int i = 0;
             for (object o = ll.First(); o != null; o = ll.Next())
@@ -109,23 +111,24 @@ namespace LL_CS
             }
 
             ll.AddHead("abc");
-            Console.WriteLine("Removing Head of one-element list");
+            Console.WriteLine("\nRemoving Head of one-element list");
             ll.RemoveHead();
             ll.AddHead("abc");
-            Console.WriteLine("Removing Tail of one-element list");
+            Console.WriteLine("\nRemoving Tail of one-element list");
             ll.RemoveTail();
 
-            Console.WriteLine("Finished testing edgecases!");
+            Console.WriteLine("\nFinished testing edgecases!");
         }
 
         static void testLinkedList_insertSorted()
         {
+            Console.WriteLine("\n\n### testLinkedList_insertSorted");
             LinkedList ll = new LinkedList();
             ll.AddTail("a");
             ll.AddTail("b");
             ll.AddTail("d");
             ll.AddTail("e");
-            Console.WriteLine("Insert \"c\" into LinkedList:");
+            Console.WriteLine("\nInsert \"c\" into LinkedList:");
             ll.Print();
             ll.InsertSorted("c", StringComparer.Ordinal);
             ll.Print();
@@ -134,7 +137,7 @@ namespace LL_CS
                 ll.RemoveHead();
 
             ll.AddTail("a");
-            Console.WriteLine("Insert \"c\" into LinkedList:");
+            Console.WriteLine("\nInsert \"c\" into LinkedList:");
             ll.Print();
             ll.InsertSorted("c", StringComparer.Ordinal);
             ll.Print();
@@ -143,7 +146,7 @@ namespace LL_CS
                 ll.RemoveHead();
 
             ll.AddTail("e");
-            Console.WriteLine("Insert \"c\" into LinkedList:");
+            Console.WriteLine("\nInsert \"c\" into LinkedList:");
             ll.Print();
             ll.InsertSorted("c", StringComparer.Ordinal);
             ll.Print();
@@ -151,11 +154,11 @@ namespace LL_CS
             while (ll.Count() > 0)
                 ll.RemoveHead();
 
-            Console.WriteLine("Insert \"c\" into empty LinkedList:");
+            Console.WriteLine("\nInsert \"c\" into empty LinkedList:");
             ll.InsertSorted("c", StringComparer.Ordinal);
             ll.Print();
 
-            Console.WriteLine("Finished testing InsertSorted!");
+            Console.WriteLine("\nFinished testing InsertSorted!");
         }
 
         static void Main(string[] args)
